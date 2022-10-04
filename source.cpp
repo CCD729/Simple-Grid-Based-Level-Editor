cebf2c5832f22a94da7590970deecf4dda308877
ccd729 <funbaba@live.com>
2022-10-04 02:05:18 -0700

    Make current tile selection follow mouse

--- source.cpp
+++ source.cpp
@@ -6,31 +6,37 @@
 #include <SFML/OpenGL.hpp>
 #include <SFML/Main.hpp>
 #include <iostream>
-//Namespaces
+// Namespaces
 using namespace sf;
 
-//Global Variables
+
 Texture tileTexture;
 Sprite tileSprite;
+
 bool saveRelease = true;
 
-//Prototypes (we should have a load asset function)
+// Prototypes
+void loadAsset();
 void handleInput(RenderWindow& window, Event& e);
 void update(RenderWindow& window);
 void render(RenderWindow& window);
 
 int main()
 {
-    RenderWindow window(VideoMode(840, 700), "Level Editor");
-    //Sprite
-    if (!tileTexture.loadFromFile("Tileset\\Platformer\\70x70\\Platformer-0.png")) {
-        return -1;
-    }
+    // Create the window
+    ContextSettings settings;
+    settings.antialiasingLevel = 8;
+    RenderWindow window(VideoMode(840, 700), "My Level Editor", Style::Default, settings);
+    window.setFramerateLimit(60);
 
+    // First load assets
+    loadAsset();
+
+    // Setup the sprite
     tileSprite.setTexture(tileTexture);
-    tileSprite.setPosition(0, 0);
+    tileSprite.setPosition(0,0);
 
-    //window event and ordered functions
+    // Window event and ordered functions
     while (window.isOpen())
     {
         Event event;
@@ -44,22 +50,40 @@ int main()
     return 0;
 }
 
+void loadAsset() {
+    //Sprite
+    if (!tileTexture.loadFromFile("Tileset\\Platformer-0.png")) {
+        std::cerr << "Error: Failed to load texture file";
+        exit(-1);
+    }
+}
+
 // Input handling
 void handleInput(RenderWindow& window, Event& e) {
+    // Window close Handler
     if (e.type == Event::Closed)
         window.close();
+
+    // Mouse Click 
     if (Mouse::isButtonPressed(Mouse::Left)) {
     }
-    if (e.type == sf::Event::KeyReleased) {
-        if (e.key.code == sf::Keyboard::F2)
+
+    // Avoid multiple triggers while holding
+    if (e.type == Event::KeyReleased) {
+        if (e.key.code == Keyboard::F2)
         {
             saveRelease = true;
         }
     }
-    // F2 key saves a screenshot
-    if (saveRelease && sf::Keyboard::isKeyPressed(sf::Keyboard::F2)) {
+    // Display current tile location following mouse
+    else if (e.type == Event::MouseMoved) {
+        tileSprite.setPosition(Mouse::getPosition(window).x / 70 * 70, Mouse::getPosition(window).y / 70 * 70);
+    }
+
+    // F2 key saves a screenshot. TODO: Maybe hide current cursor-following tile?
+    if (saveRelease && Keyboard::isKeyPressed(Keyboard::F2)) {
         saveRelease = false;
-        sf::Texture texture;
+        Texture texture;
         texture.create(window.getSize().x, window.getSize().y);
         texture.update(window);
         if (texture.copyToImage().saveToFile("Mylevel.png")) {
@@ -67,9 +91,11 @@ void handleInput(RenderWindow& window, Event& e) {
         }
     }
 }
+// What happens in the game frame
 void update(RenderWindow& window) {
 
 }
+// What to render on screen
 void render(RenderWindow& window) {
     window.clear();
     window.draw(tileSprite);
